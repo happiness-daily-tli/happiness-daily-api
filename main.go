3a25771460811769e6ad904c4fd569b4a911d393
Connor Kim <k.connor614@gmail.com>
2021-07-23 23:20:17 +0900

    gin server init

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	r := gin.Default()
 
-	fmt.Println("Goods")
+	r.Use(gin.Logger())
 
-	//r := gin.Default()
+	r.GET("/ping", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "goods",
+		})
+	})
+
+	err := r.Run()
+	if err != nil {
+		return
+	}
 }
